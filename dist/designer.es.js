43874d2de6833e20e6a040b9ea7871389bd7cf85
Sean Tsang <seantsang83@gmail.com>
2023-06-30 09:49:59 +0800

    update build

--- dist/designer.es.js
+++ dist/designer.es.js
@@ -61711,13 +61711,13 @@ function registerIcon(app) {
 if (typeof window !== "undefined") {
   let loadSvg = function() {
     var body = document.body;
-    var svgDom = document.getElementById("__svg__icons__dom__1687763267098__");
+    var svgDom = document.getElementById("__svg__icons__dom__1688089781135__");
     if (!svgDom) {
       svgDom = document.createElementNS("http://www.w3.org/2000/svg", "svg");
       svgDom.style.position = "absolute";
       svgDom.style.width = "0";
       svgDom.style.height = "0";
-      svgDom.id = "__svg__icons__dom__1687763267098__";
+      svgDom.id = "__svg__icons__dom__1688089781135__";
       svgDom.setAttribute("xmlns", "http://www.w3.org/2000/svg");
       svgDom.setAttribute("xmlns:link", "http://www.w3.org/1999/xlink");
     }
