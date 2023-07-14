6995fe397da84250c54d48a0ff51940ee35a86e6
Oy <OY.Ou@weltronics.com>
2023-07-14 10:31:17 +0800

    build

--- dist/designer.es.js
+++ dist/designer.es.js
@@ -61975,13 +61975,13 @@ function registerIcon(app) {
 if (typeof window !== "undefined") {
   let loadSvg = function() {
     var body = document.body;
-    var svgDom = document.getElementById("__svg__icons__dom__1689301655846__");
+    var svgDom = document.getElementById("__svg__icons__dom__1689301846547__");
     if (!svgDom) {
       svgDom = document.createElementNS("http://www.w3.org/2000/svg", "svg");
       svgDom.style.position = "absolute";
       svgDom.style.width = "0";
       svgDom.style.height = "0";
-      svgDom.id = "__svg__icons__dom__1689301655846__";
+      svgDom.id = "__svg__icons__dom__1689301846547__";
       svgDom.setAttribute("xmlns", "http://www.w3.org/2000/svg");
       svgDom.setAttribute("xmlns:link", "http://www.w3.org/1999/xlink");
     }
