07280a3cb379be2167ebec7a9e808c6e535133b6
oy <OY.Ou@weltronics.com>
2024-11-27 17:10:48 +0800

    3.0.57

--- dist/render.es.js
+++ dist/render.es.js
@@ -31493,13 +31493,13 @@ function registerIcon(app) {
 if (typeof window !== "undefined") {
   let loadSvg = function() {
     var body = document.body;
-    var svgDom = document.getElementById("__svg__icons__dom__1732692320858__");
+    var svgDom = document.getElementById("__svg__icons__dom__1732698443536__");
     if (!svgDom) {
       svgDom = document.createElementNS("http://www.w3.org/2000/svg", "svg");
       svgDom.style.position = "absolute";
       svgDom.style.width = "0";
       svgDom.style.height = "0";
-      svgDom.id = "__svg__icons__dom__1732692320858__";
+      svgDom.id = "__svg__icons__dom__1732698443536__";
       svgDom.setAttribute("xmlns", "http://www.w3.org/2000/svg");
       svgDom.setAttribute("xmlns:link", "http://www.w3.org/1999/xlink");
     }
