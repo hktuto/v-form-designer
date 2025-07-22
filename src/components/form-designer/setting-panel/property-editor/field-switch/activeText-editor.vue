1eac6c142d69573567b17df2b2f6ace0b7f50b3e
oy <OY.Ou@weltronics.com>
2025-07-22 10:08:32 +0800

    inactiveText

--- src/components/form-designer/setting-panel/property-editor/field-switch/activeText-editor.vue
+++ src/components/form-designer/setting-panel/property-editor/field-switch/activeText-editor.vue
@@ -1,23 +1,38 @@
 <template>
   <el-form-item :label="$t('designer.setting.activeText')">
-    <el-input v-model="optionModel.activeText"></el-input>
+    <el-select
+      v-model="optionModel.activeText"
+      filterable
+      allow-create
+      default-first-option
+    >
+      <el-option
+        v-for="(item, idx) in activeTextList"
+        :key="idx"
+        :label="item"
+        :value="item"
+      ></el-option>
+    </el-select>
   </el-form-item>
 </template>
 
 <script>
-  import i18n from "@/utils/i18n"
+import i18n from "@/utils/i18n";
 
-  export default {
-    name: "activeText-editor",
-    mixins: [i18n],
-    props: {
-      designer: Object,
-      selectedWidget: Object,
-      optionModel: Object,
-    },
-  }
+export default {
+  name: "activeText-editor",
+  mixins: [i18n],
+  props: {
+    designer: Object,
+    selectedWidget: Object,
+    optionModel: Object,
+  },
+  data() {
+    return {
+      activeTextList: ["actions.inactive", "actions.activate"],
+    };
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/form-designer/setting-panel/property-editor/field-switch/inactiveText-editor.vue
+++ src/components/form-designer/setting-panel/property-editor/field-switch/inactiveText-editor.vue
@@ -1,23 +1,38 @@
 <template>
   <el-form-item :label="$t('designer.setting.inactiveText')">
-    <el-input v-model="optionModel.inactiveText"></el-input>
+    <el-select
+      v-model="optionModel.inactiveText"
+      filterable
+      allow-create
+      default-first-option
+    >
+      <el-option
+        v-for="(item, idx) in inactiveTextList"
+        :key="idx"
+        :label="item"
+        :value="item"
+      ></el-option>
+    </el-select>
   </el-form-item>
 </template>
 
 <script>
-  import i18n from "@/utils/i18n"
+import i18n from "@/utils/i18n";
 
-  export default {
-    name: "inactiveText-editor",
-    mixins: [i18n],
-    props: {
-      designer: Object,
-      selectedWidget: Object,
-      optionModel: Object,
-    },
-  }
+export default {
+  name: "inactiveText-editor",
+  mixins: [i18n],
+  props: {
+    designer: Object,
+    selectedWidget: Object,
+    optionModel: Object,
+  },
+  data() {
+    return {
+      inactiveTextList: ["actions.inactive", "actions.activate"],
+    };
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
